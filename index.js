1c6ae66ea73b237e7e006c93af3278eb30ed7c10
Melanie Wilson <melaniemwilson1@gmail.com>
2023-03-27 14:19:39 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    names[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return names;
+}
+
+function countDown(a) {
+    let i = a;
+    while (a >= 0){
+        console.log(a);
+        a--;
+    }
+}
